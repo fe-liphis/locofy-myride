aa79c9178fbb523a65e369b8ccaf2d1c62e94aaa
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-22 15:08:28 -0300

    FEAT: Adicionada a rota principal do router com seus respectivos components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LayoutPage from "./pages/LayoutPages/LayoutPage";
+import Home from "./pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
