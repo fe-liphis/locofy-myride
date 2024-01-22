670058f79a6cb4472b85138fc2901120376f8c32
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-22 15:40:41 -0300

    FEAT: Esqueleto do footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,71 @@
+function Footer() {
+  return (
+    <footer>
+      <article>
+        <h1>LOGO</h1>
+        <p>MyRide Inc., 2nd Floor, New York, NY 10016</p>
+        <ul>
+          <li>
+            <a href="">Face</a>
+          </li>
+          <li>
+            <a href="">Insta</a>
+          </li>
+          <li>
+            <a href="">Twitter</a>
+          </li>
+        </ul>
+      </article>
+      <section>
+        <h5>Company</h5>
+        <ul>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">Careers</a>
+          </li>
+          <li>
+            <a href="">How we work</a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h5>Support</h5>
+        <ul>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">US Office</a>
+          </li>
+          <li>
+            <a href="">Asia Office</a>
+          </li>
+          <li>
+            <a href="">Help Center</a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h5>More</h5>
+        <ul>
+          <li>
+            <a href="">Become a partner</a>
+          </li>
+          <li>
+            <a href="">Partner Support</a>
+          </li>
+          <li>
+            <a href="">Mobile app links</a>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/RootLayout/RootLayout.tsx
+++ frontend/src/pages/RootLayout/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function RootLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
