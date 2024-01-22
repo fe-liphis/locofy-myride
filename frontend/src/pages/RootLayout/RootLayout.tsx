54658043a08ef41b112bf10b3b9da3ea02019904
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-22 15:53:36 -0300

    FEAT: Adicionado o fundo do App com o GlobalStyle

--- frontend/src/pages/RootLayout/RootLayout.tsx
+++ frontend/src/pages/RootLayout/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import GlobalStyle from "../../GlobalStyle";
 
 function RootLayout() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Outlet />
       <Footer />
