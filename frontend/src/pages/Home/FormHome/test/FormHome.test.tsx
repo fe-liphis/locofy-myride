2ed54ae1ac92d8c908995ce9b10a6332ae2343bf
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-26 15:04:43 -0300

    FEAT: Teste adicionado do RootLayout

--- frontend/src/pages/Home/FormHome/test/FormHome.test.tsx
+++ frontend/src/pages/Home/FormHome/test/FormHome.test.tsx
@@ -1,5 +1,25 @@
+import { test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import MyThemeProvider from "../../../../styles/MyThemeProvider";
+import FormHome from "../FormHome";
+
 describe("display FormHome component", () => {
-    test("header", () => {
-        
-    })
-})
+  test("header", () => {
+    render(
+      <MyThemeProvider>
+        <BrowserRouter>
+          <FormHome />
+        </BrowserRouter>
+      </MyThemeProvider>
+    );
+
+    const title = screen.getByText(/Drive with MyRide/i);
+    const subTitle = screen.getByText(
+      /Register as a driver using the form below. Our team will assess and get back to you within 48 hours./i
+    );
+
+    expect(title).not.toBeNull();
+    expect(subTitle).not.toBeNull();
+  });
+});
---
+++ frontend/src/pages/RootLayout/test/RootLayout.test.tsx
@@ -0,0 +1,25 @@
+import { test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import MyThemeProvider from "../../../styles/MyThemeProvider";
+import RootLayout from "../RootLayout";
+
+test("display RootLayout", () => {
+  render(
+    <MyThemeProvider>
+      <BrowserRouter>
+        <RootLayout />
+      </BrowserRouter>
+    </MyThemeProvider>
+  );
+
+  const home = screen.getByText(/home/i);
+  const gettingTaxi = screen.getByText(/Getting a Taxi/i);
+  const mobileApp = screen.getAllByText(/Mobile App/i);
+  const homeLink = screen.getByText(/home/i);
+
+  expect(home).not.toBeNull();
+  expect(gettingTaxi).not.toBeNull();
+  expect(mobileApp).not.toBeNull();
+  expect(homeLink).not.toBeNull();
+});
