d3a2e6794c169278e77d9a84ad0bb4f05f44c850
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-26 16:35:26 -0300

    FEAT: Mais testes adicionados ao utils.test.tsx

--- frontend/src/utils/test/utils.test.tsx
+++ frontend/src/utils/test/utils.test.tsx
@@ -7,15 +7,71 @@ test("get first name of Felipe Gabriel", () => {
   expect(getFirstName("Felipe Gabriel")).toBe("Felipe");
 });
 
-test("get car type formated of suvVan", () => {
-  expect(getCarType("suvVan")).toBe("SUV/Van");
+describe("get car type", () => {
+  test("formated of suvVan", () => {
+    expect(getCarType("suvVan")).toBe("SUV/Van");
+  });
+
+  test("formated of sedan", () => {
+    expect(getCarType("sedan")).toBe("Sedan");
+  });
+
+  test("formated of semiLuxury", () => {
+    expect(getCarType("semiLuxury")).toBe("Semi Luxury");
+  });
+
+  test("formated of luxuryCar", () => {
+    expect(getCarType("luxuryCar")).toBe("Luxury Car");
+  });
+
+  test("on error", () => {
+    expect(getCarType("Error")).toBe("Error");
+  });
 });
 
-test("returns Sedan icon for sedan type", () => {
-  const Sedan = getTypeCarIcon("sedan");
-  render(Sedan);
+describe("display all icons of getTypeCarIcon", () => {
+  test("returns Sedan icon for sedan type", () => {
+    const Sedan = getTypeCarIcon("sedan");
+    render(Sedan);
 
-  const icon = screen.getByLabelText("Sedan Car Icon");
+    const icon = screen.getByLabelText("Sedan Car Icon");
 
-  expect(icon).not.toBeNull();
+    expect(icon).not.toBeNull();
+  });
+
+  test("returns SuvVan icon for suvVan type", () => {
+    const SuvVan = getTypeCarIcon("suvVan");
+    render(SuvVan);
+
+    const icon = screen.getByLabelText("SUV Van Car Icon");
+
+    expect(icon).not.toBeNull();
+  });
+
+  test("returns SemiLuxury icon for semiLuxury type", () => {
+    const SemiLuxury = getTypeCarIcon("semiLuxury");
+    render(SemiLuxury);
+
+    const icon = screen.getByLabelText("Semi Luxury Icon");
+
+    expect(icon).not.toBeNull();
+  });
+
+  test("returns Sedan icon for luxuryCar type", () => {
+    const LuxuryCar = getTypeCarIcon("luxuryCar");
+    render(LuxuryCar);
+
+    const icon = screen.getByLabelText("Luxury Car Icon");
+
+    expect(icon).not.toBeNull();
+  });
+
+  test("returns Error", () => {
+    const Error = getTypeCarIcon("Invalid Name");
+    render(Error);
+
+    const icon = screen.getByLabelText("Error Icon");
+
+    expect(icon).not.toBeNull();
+  });
 });
