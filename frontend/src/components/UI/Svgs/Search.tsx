288cc9f47c9538d4c1d5489ac2df632bc77d9327
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-01-27 16:03:54 -0300

    FEAT: Svg Search componentizado

---
+++ frontend/src/components/UI/Svgs/Search.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Search() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      fill="none"
+      viewBox="0 0 24 25"
+    >
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M15.5 14.367h-.79l-.28-.27A6.471 6.471 0 0016 9.867a6.5 6.5 0 10-6.5 6.5c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99 1.49-1.49-4.99-5zm-6 0c-2.49 0-4.5-2.01-4.5-4.5s2.01-4.5 4.5-4.5 4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default Search;
